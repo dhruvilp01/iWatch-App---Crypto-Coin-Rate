c32f34a6b917b66ba5d3d3fd6efb574cb5a05b31
Dhruvil Patel <pateldhruvil01@yahoo.com>
2018-07-09 20:19:00 -0400

    Initial Commit

---
+++ Coin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coin
+//
+//  Created by Dhruvil Patel on 7/9/18.
+//  Copyright © 2018 Dhruvil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
